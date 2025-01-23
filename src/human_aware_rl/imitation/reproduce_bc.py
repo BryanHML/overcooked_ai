b186de68dae53bb31704b94192dd4e377ee3a0a8
BryanHML <bryanho146@gmail.com>
2025-01-23 14:44:51 +1100

    edit code to only train BC model for new cramped_room

--- src/human_aware_rl/imitation/reproduce_bc.py
+++ src/human_aware_rl/imitation/reproduce_bc.py
@@ -17,12 +17,31 @@ if __name__ == "__main__":
     # however when loading layouts there are no random0/3
     # The same parameter is used in both setting up the layout for training and loading the corresponding trajectories
     # so without modifying the dataframes, I have to create new layouts
+    # for layout in [
+    #     "random3",
+    #     "coordination_ring",
+    #     "cramped_room",
+    #     "random0",
+    #     "asymmetric_advantages",
+    # ]:
+    #     current_file_dir = os.path.dirname(os.path.abspath(__file__))
+    #     # this is where
+    #     bc_dir = os.path.join(current_file_dir, "bc_runs", "train", layout)
+    #     if os.path.isdir(bc_dir):
+    #         # if this bc agent has been created, we continue to the next layout
+    #         continue
+    #     params_to_override = {
+    #         "layouts": [layout],
+    #         "layout_name": layout,
+    #         "data_path": CLEAN_2019_HUMAN_DATA_TRAIN,
+    #         "epochs": 100,
+    #         "old_dynamics": True,
+    #     }
+    #     bc_params = get_bc_params(**params_to_override)
+    #     train_bc_model(bc_dir, bc_params, True)
+
     for layout in [
-        "random3",
-        "coordination_ring",
-        "cramped_room",
-        "random0",
-        "asymmetric_advantages",
+        "cramped_room"
     ]:
         current_file_dir = os.path.dirname(os.path.abspath(__file__))
         # this is where
